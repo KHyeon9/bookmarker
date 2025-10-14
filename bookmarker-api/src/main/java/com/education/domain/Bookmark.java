b25a50d931edfdf1d417976cf4fbb6fc6a4c0e20
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-10-14 23:54:45 +0900

    feet : Bookmark domain 작성

---
+++ bookmarker-api/src/main/java/com/education/domain/Bookmark.java
@@ -0,0 +1,29 @@
+package com.education.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookmarks")
+public class Bookmark {
+    @Id
+    @SequenceGenerator(name = "bm_id_seq_gen", sequenceName = "bm_id_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bm_id_seq_gen")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String url;
+
+    private Instant createdAt;
+}
