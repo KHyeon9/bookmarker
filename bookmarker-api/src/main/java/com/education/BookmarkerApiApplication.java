e3fcccf539330523c73d8fdaa0c0807db407e83a
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-10-15 19:31:35 +0900

    feet : 주석 내용 수정

--- bookmarker-api/src/main/java/com/education/BookmarkerApiApplication.java
+++ bookmarker-api/src/main/java/com/education/BookmarkerApiApplication.java
@@ -3,6 +3,8 @@ package com.education;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// github action을 통해서 CI구출
+// CI를 통해서 자동으로 변경이 발생하면 DockerHub에 이미지를 자동으로 배포함
 @SpringBootApplication
 public class BookmarkerApiApplication {
 
