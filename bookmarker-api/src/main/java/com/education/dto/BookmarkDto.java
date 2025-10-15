f60b9ec44b42674b703402c1ff5a8567181018c3
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-10-15 16:40:18 +0900

    feet : Entity와 분리하기 위한 BookmarkDto 생성

---
+++ bookmarker-api/src/main/java/com/education/dto/BookmarkDto.java
@@ -0,0 +1,29 @@
+package com.education.dto;
+
+import com.education.domain.Bookmark;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookmarkDto {
+    private Long id;
+    private String title;
+    private String url;
+    private Instant createdAt;
+
+    public static BookmarkDto fromEntity(Bookmark bookmark) {
+        return new BookmarkDto(
+                bookmark.getId(),
+                bookmark.getTitle(),
+                bookmark.getUrl(),
+                bookmark.getCreatedAt()
+        );
+    }
+}
