22e7bfd702cc01fd5d527d24294bf233fcfaa19a
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-10-22 20:21:47 +0900

    feet : pod가 h2로 연결 문제 확인을 위한 수정

--- bookmarker-api/src/main/java/com/education/BookmarkerApiApplication.java
+++ bookmarker-api/src/main/java/com/education/BookmarkerApiApplication.java
@@ -2,6 +2,7 @@ package com.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 // github action을 통해서 CI구출
 // CI를 통해서 자동으로 변경이 발생하면 DockerHub에 이미지를 자동으로 배포함
@@ -9,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookmarkerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmarkerApiApplication.class, args);
+
+//		SpringApplication.run(BookmarkerApiApplication.class, args);
+		SpringApplication app = new SpringApplication(BookmarkerApiApplication.class);
+		ConfigurableApplicationContext ctx = app.run(args);
+
+		System.out.println("datasource url = " + ctx.getEnvironment().getProperty("spring.datasource.url"));
 	}
 
 }
